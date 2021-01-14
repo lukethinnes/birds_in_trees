d20144b9dec0f37701f588dec3699677cfa1a3e1
Luke Thinnes <lukethinnes@yahoo.com>
2021-01-14 10:58:00 -0700

    done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :branches, only: [:index, :show, :create, :update, :destroy]
-  resources :trees, only: [:index]
-  resources :birds, only: [:index]
+  resources :trees, only: [:index, :show, :create]
+  resources :birds, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
